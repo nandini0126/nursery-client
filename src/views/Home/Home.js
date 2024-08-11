4851aca5446249d77adb093ef3061a875ce6bc8c
nandini0126 <nandiniyamale26@gmail.com>
2024-08-11 20:03:32 +0530

    changes

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -9,7 +9,7 @@ function Home() {
 
     const loadPlants= async ()=>{
         toast.loading("Loading plants...")
-        const response = await axios.get(`https://nursery-0u93.onrender.com/plants`)
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}/plants`)
         toast.dismiss()
 
         toast.success("Plants loaded successfully")
